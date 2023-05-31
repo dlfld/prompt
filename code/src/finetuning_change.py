8d7f6af59707ac88040e8715b06b88c013275edf
戴林峰 <dailinfeng66@163.com>
2023-05-31 20:50:28 +0800

    update code

--- code/src/finetuning_change.py
+++ code/src/finetuning_change.py
@@ -86,7 +86,7 @@ def train_model(train_data, test_data,model,tokenizer):
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
-            epochs.set_description("Epoch (Loss=%g)" % round(total_loss/Config.batch_size, 5))
+            epochs.set_description("Epoch (Loss=%g)" % round(loss/Config.batch_size, 5))
             loss.cpu()
             bert_loss.cpu()
             del loss
