23a80dcdb95c7b0d7a8c6ece6fedffaf0437dfb9
戴林峰 <dailinfeng66@163.com>
2023-06-05 14:32:19 +0800

    update code

--- code/src/crf/model_params.py
+++ code/src/crf/model_params.py
@@ -18,9 +18,9 @@ class Config(object):
     # batch_size
     batch_size = 32
     # 学习率
-    learning_rate = 2e-7
+    learning_rate = 2e-4
     # epoch数
-    num_train_epochs = 150
+    num_train_epochs = 100
     # 句子的最大补齐长度
     # sentence_max_len = 2048
     sentence_max_len = 128
