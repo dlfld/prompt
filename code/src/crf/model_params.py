31f77c1e94663a4b586091efbff42b8f280694b9
戴林峰 <dailinfeng66@163.com>
2023-06-13 14:12:04 +0800

    update code

--- code/src/crf/model_params.py
+++ code/src/crf/model_params.py
@@ -17,7 +17,9 @@ class Config(object):
     # bert
     # model_checkpoint = "/home/dlf/prompt/code/model/bert_large_chinese"
     # medbert
-    model_checkpoint = "/home/dlf/prompt/code/model/medbert"
+    # model_checkpoint = "/home/dlf/prompt/code/model/medbert"
+    # bart
+    model_checkpoint = "/home/dlf/prompt/code/model/bart-large"
     # batch_size
     batch_size = 32
     # 学习率
