2e4c39dc9534a5406ce66de442412307914d313d
戴林峰 <dailinfeng66@163.com>
2023-06-18 16:04:37 +0800

    update code

--- code/src/crf/model_params.py
+++ code/src/crf/model_params.py
@@ -15,13 +15,13 @@ class Config(object):
     # train_dataset_path = "/home/dlf/prompt/dataset.csv"
     # 预训练模型的位置
     # bert
-    # model_checkpoint = "/home/dlf/prompt/code/model/bert_large_chinese"
+    model_checkpoint = "/home/dlf/prompt/code/model/bert_large_chinese"
     # medbert
     # model_checkpoint = "/home/dlf/prompt/code/model/medbert"
     # bart
-    model_checkpoint = "/home/dlf/prompt/code/model/bart-large"
+    # model_checkpoint = "/home/dlf/prompt/code/model/bart-large"
     # batch_size
-    batch_size = 4
+    batch_size = 1
     # 学习率
     learning_rate = 2e-5
     # epoch数
@@ -39,9 +39,9 @@ class Config(object):
     # k折交叉验证
     kfold = 10
     # 1train9test 10折 train path
-    train_1_9_path = "/home/dlf/prompt/code/data/split_data/1_9_split/train_{idx}"
+    train_1_9_path = "/home/dlf/prompt/code/data/split_data/1_9_split/train_{idx}.data"
     # 1train9test 10折 test path
-    test_1_9_path = "/home/dlf/prompt/code/data/split_data/1_9_split/test_{idx}"
+    test_1_9_path = "/home/dlf/prompt/code/data/split_data/1_9_split/test_{idx}.data"
 
     # 是否断点续训
     resume = True
