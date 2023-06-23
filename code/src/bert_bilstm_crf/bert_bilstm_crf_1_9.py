fef22af6edb4bca7d504b3ae125e76d40d7a9df0
戴林峰 <dailinfeng66@163.com>
2023-06-23 11:11:45 +0800

    update code

--- code/src/bert_bilstm_crf/bert_bilstm_crf_1_9.py
+++ code/src/bert_bilstm_crf/bert_bilstm_crf_1_9.py
@@ -292,11 +292,11 @@ def train(model_checkpoint):
     logddd.log(avg_prf)
 
 
-# pretrain_models = ["/home/dlf/prompt/code/model/bart-large"]
-pretrain_models = [
-    "/home/dlf/prompt/code/model/bert_large_chinese",
-    "/home/dlf/prompt/code/model/medbert",
-    "/home/dlf/prompt/code/model/bart-large"]
+pretrain_models = ["/home/dlf/prompt/code/model/bart-large"]
+# pretrain_models = [
+#     "/home/dlf/prompt/code/model/bert_large_chinese",
+#     "/home/dlf/prompt/code/model/medbert",
+#     "/home/dlf/prompt/code/model/bart-large"]
 for pretrain_model in pretrain_models:
     prf = pretrain_model
     logddd.log(prf)
