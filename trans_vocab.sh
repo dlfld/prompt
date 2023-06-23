a8f68001616c504070b66331784d2cd51b7154f0
戴林峰 <dailinfeng66@163.com>
2023-06-23 10:16:42 +0800

    update code

---
+++ trans_vocab.sh
@@ -0,0 +1,3 @@
+scp code/model/bart-large/vocab* dlf@$1:~/prompt/code/model/bart-large/
+scp code/model/bert_large_chinese/vocab* dlf@$1:~/prompt/code/model/bert_large_chinese/
+scp code/model/medbert/vocab* dlf@$1:~/prompt/code/model/medbert/
