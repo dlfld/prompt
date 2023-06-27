e6cd931c90539e547fd506174f4f70b3cb436e08
戴林峰 <dailinfeng66@163.com>
2023-06-27 13:53:55 +0800

    update code

--- code/src/prompt/model_params.py
+++ code/src/prompt/model_params.py
@@ -29,7 +29,7 @@ class Config(object):
     num_train_epochs = 100
     # 句子的最大补齐长度
     # sentence_max_len = 2048
-    sentence_max_len = 128
+    sentence_max_len = 256
     # 结果文件存储位置
     predict_res_file = "/home/dlf/prompt/code/res_files/short_data_res_{}.txt"
     # 词性的类别数量
