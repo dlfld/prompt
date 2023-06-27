f8e09e4d6bbf8a2d1570ec60f483dcbce19b2a9a
戴林峰 <dailinfeng66@163.com>
2023-06-27 22:32:56 +0800

    update code

--- code/src/prompt/main.py
+++ code/src/prompt/main.py
@@ -131,10 +131,12 @@ def train(model_checkpoint, few_shot_start, data_index):
     # 加载test标准数据
     standard_data_test = joblib.load(Config.test_data_path)
     _, data_proce_tokenizer = load_model(model_checkpoint)
-    test_data_instances = load_instance_data(standard_data_test, data_proce_tokenizer, Config, is_train_data=False)
-    joblib.dump(test_data_instances, "test_data_instances.data")
-    print("DONE!")
-    exit(0)
+    test_data_instances = None
+    if os.path.exists("bert_test_data_instances.data"):
+        test_data_instances = joblib.load("medbert_test_data_instances.data")
+    # test_data_instances = load_instance_data(standard_data_test, data_proce_tokenizer, Config, is_train_data=False)
+    # joblib.dump(test_data_instances, "medbert_test_data_instances.data")
+
     # 对每一个数量的few-shot进行kfold交叉验证
     for few_shot_idx in range(few_shot_start, len(Config.few_shot)):
         item = Config.few_shot[few_shot_idx]
