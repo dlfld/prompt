a12c434060217b0bd1032ae1256ab6c4f36b163b
戴林峰 <dailinfeng66@163.com>
2023-06-28 10:28:15 +0800

    update code

--- code/src/bert_bilstm_crf/bert_bilstm_crf.py
+++ code/src/bert_bilstm_crf/bert_bilstm_crf.py
@@ -345,10 +345,10 @@ def train(model_checkpoint, few_shot_start, data_index):
 for pretrain_model in Config.pretrain_models:
     prf = pretrain_model
     logddd.log(prf)
-    if os.path.exists("checkpoint_outer.data") and Config.resume:
-        check_point_outer = joblib.load("check_point_outer")
-        os.rename("checkpoint_outer.data", "checkpoint_outer_older.data")
-        if check_point_outer['model'] == pretrain_model:
-            train(pretrain_model, check_point_outer["few_shot_idx"], check_point_outer["train_data_idx"])
-            continue
-    train(pretrain_model, 0, 0)
+    # if os.path.exists("checkpoint_outer.data") and Config.resume:
+    #     check_point_outer = joblib.load("check_point_outer")
+    #     os.rename("checkpoint_outer.data", "checkpoint_outer_older.data")
+    #     if check_point_outer['model'] == pretrain_model:
+    #         train(pretrain_model, check_point_outer["few_shot_idx"], check_point_outer["train_data_idx"])
+    #         continue
+    train(pretrain_model, 1, 0)
