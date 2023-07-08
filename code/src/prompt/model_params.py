84b9e8de8fb6a5fc24150382add770eb5b9393ce
戴林峰 <dailinfeng66@163.com>
2023-07-08 14:57:43 +0800

    update code

--- code/src/prompt/model_params.py
+++ code/src/prompt/model_params.py
@@ -34,9 +34,9 @@ class Config(object):
     # 是否断点续训
     resume = False
     # few-shot 划分的数量
-    # few_shot = [5, 10, 15, 20, 25]
+    few_shot = [5, 10, 15, 20, 25]
     # few_shot = [5, 10, 15, 20, 25, 50, 75, 100, 200, 500]
-    few_shot = [50, 75, 100, 200, 500]
+    # few_shot = [50, 75, 100, 200, 500]
     # few_shot = [50, 70]
 
     # 测试集位置
