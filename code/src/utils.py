9c780fb04ec3e5a3d583e1c8a0efc841b58f5643
戴林峰 <dailinfeng66@163.com>
2023-07-09 09:13:02 +0800

    add ctb

--- code/src/utils.py
+++ code/src/utils.py
@@ -67,7 +67,8 @@ class EarlyStopping:
         '''Saves model when validation loss decrease.'''
         if self.verbose:
             print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        path = os.path.join(self.save_path, 'best_network.pth')
+        # path = os.path.join(self.save_path, 'best_network.pth')
+        path = self.save_path
         torch.save(model.state_dict(), path)	# 这里会存储迄今最优模型的参数
         self.val_loss_min = val_loss
 
