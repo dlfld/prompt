f11d76aee8fdb2d6cdb5635e5a8cc8359f804a11
dlf.3901 <dailinfeng66@163.com>
2023-07-24 14:37:52 +0800

    update code

--- code/src/crf/model_params.py
+++ code/src/crf/model_params.py
@@ -54,8 +54,8 @@ class Config(object):
     # 是否断点续训
     resume = True
     # few-shot 划分的数量
-    # few_shot = [5, 10, 15, 20, 25, 50, 75, 100, 200, 500]
-    few_shot = [500]
+    few_shot = [5, 10, 15, 20, 25, 50, 75, 100, 200, 500]
+    # few_shot = [500]
     # few_shot = [50, 70]
 
     # 测试集位置
