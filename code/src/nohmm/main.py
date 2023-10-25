590d36d722bedb2da191dd927afa47854c970a10
dlf 4901 <dailinfeng66@163.com>
2023-10-25 21:50:31 +0800

    update code

--- code/src/nohmm/main.py
+++ code/src/nohmm/main.py
@@ -192,7 +192,7 @@ def train(model_checkpoint, few_shot_start, data_index):
     else:
         test_data_instances = load_instance_data(standard_data_test, tokenizer_test, Config, is_train_data=False)
         joblib.dump(test_data_instances,instance_filename)
-
+    #test_data_instances = test_data_instances[:40]
     # logddd.log(tokenizer_test.convert_ids_to_tokens(test_data_instances[0]["input_ids"][0]))
     # logddd.log(tokenizer_test.convert_tokens_to_ids(test_data_instances[0]["labels"][0]))
     # exit(0)
@@ -284,4 +284,4 @@ for pretrain_model in Config.pretrain_models:
     #         continue
     
     pre_train_model_name = pretrain_model.split("/")[-1]
-    train(pretrain_model, 0, 1)
+    train(pretrain_model, 0, 0)
