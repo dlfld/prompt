1480e97b06da7b74430b022089bc21b7fbbdcf51
dlf 4901 <dailinfeng66@163.com>
2023-11-16 14:53:30 +0800

    update code

--- code/src/nohmm/model_params.py
+++ code/src/nohmm/model_params.py
@@ -7,11 +7,7 @@ class Config(object):
     # 学习率
     learning_rate = 2e-5
     # epoch数
-<<<<<<< HEAD
     num_train_epochs = 40
-=======
-    num_train_epochs = 100
->>>>>>> a116b8daabaabeda8d463aa84bf7b0a315e92d3e
     # 句子的最大补齐长度
     # sentence_max_len = 2048
     sentence_max_len = 256
@@ -43,23 +39,23 @@ class Config(object):
     # 是否断点续训
     resume = False
     # few-shot 划分的数量
-    # few_shot = [10,15,20,25]
+    few_shot = [5,10,15,20,25]
     # few_shot = [5, 10, 15, 20, 25, 50, 75, 100, 200, 500]
     # few_shot = [50, 75, 100, 200, 500]
     # few_shot = [50, 70]
     # few_shot = [5, 10, 15, 20, 25, 50, 75, 100, 200, 500]
-    few_shot = [ 25]
+    # few_shot = [ 25]
     # 测试集位置
     # jw
     # train_data_path = "/home/dlf/prompt/code/data/split_data/fold/{item}.data"
     # test_data_path = "/home/dlf/prompt/code/data/split_data/pos_seg_test.data"
     # CTB
-    test_data_path = "/home/dlf/prompt/code/data/ctb/split_data/few_shot/one_tentn_test_datas.data"
-    train_data_path = "/home/dlf/prompt/code/data/ctb/split_data/few_shot/fold/{item}.data"
+    # test_data_path = "/home/dlf/prompt/code/data/ctb/split_data/few_shot/one_tentn_test_datas.data"
+    # train_data_path = "/home/dlf/prompt/code/data/ctb/split_data/few_shot/fold/{item}.data"
     # test_data_path = "/home/dlf/prompt/code/data/ctb/split_data/few_shot/test_3000.data"
     # UD数据集
-    # test_data_path = "/home/dlf/prompt/code/data/ud/ud_en/test.data"
-    # train_data_path = "/home/dlf/prompt/code/data/ud/ud_en/fold/{item}.data"
+    test_data_path = "/home/dlf/prompt/code/data/ud/ud_en/test.data"
+    train_data_path = "/home/dlf/prompt/code/data/ud/ud_en/fold/{item}.data"
     # log dir
     log_dir = "ctb_nohmm/"
     # train dataset template
@@ -73,20 +69,20 @@ class Config(object):
     #                   "SP", "VV", "M", "PU", "CD", "BP", "JJ", "LC", "VC",
     #                   "VA", "VE"]
     # ctb数据集
-    special_labels = ["[PLB]", "NR", "NN", "AD", "PN", "OD", "CC", "DEG",
-                      "SP", "VV", "M", "PU", "CD", "BP", "JJ", "LC", "VC",
-                      "VA", "VE",
-                      "NT-SHORT", "AS-1", "PN", "MSP-2", "NR-SHORT", "DER",
-                      "URL", "DEC", "FW", "IJ", "NN-SHORT", "BA", "NT", "MSP", "LB",
-                      "P", "NOI", "VV-2", "ON", "SB", "CS", "ETC", "DT", "AS", "M", "X",
-                      "DEV"
-                      ]
+    # special_labels = ["[PLB]", "NR", "NN", "AD", "PN", "OD", "CC", "DEG",
+    #                   "SP", "VV", "M", "PU", "CD", "BP", "JJ", "LC", "VC",
+    #                   "VA", "VE",
+    #                   "NT-SHORT", "AS-1", "PN", "MSP-2", "NR-SHORT", "DER",
+    #                   "URL", "DEC", "FW", "IJ", "NN-SHORT", "BA", "NT", "MSP", "LB",
+    #                   "P", "NOI", "VV-2", "ON", "SB", "CS", "ETC", "DT", "AS", "M", "X",
+    #                   "DEV"
+    #                   ]
     # UD 数据集
-    # special_labels = ["[PLB]","PROPN", "SYM", "X", "PRON", "ADJ", "NOUN", "PART", "DET", "CCONJ", "ADP", "VERB", "NUM", "PUNCT", "AUX", "ADV"]
+    special_labels = ["[PLB]","PROPN", "SYM", "X", "PRON", "ADJ", "NOUN", "PART", "DET", "CCONJ", "ADP", "VERB", "NUM", "PUNCT", "AUX", "ADV"]
     # 检查点的保存位置
     checkpoint_file = "/home/dlf/prompt/code/src/prompt/pths/ud-ch_{filename}.pth"
     pretrain_models = [
         #"/home/dlf/prompt/code/model/bert_large_chinese",
-        #"/home/dlf/prompt/code/model/medbert",
-         "/home/dlf/prompt/code/model/bart-large"
+        "/home/dlf/prompt/code/model/medbert",
+        #  "/home/dlf/prompt/code/model/bart-large"
     ]
