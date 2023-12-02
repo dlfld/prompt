8b0400d1672dae28b94064a8b504c6f664d46387
戴林峰 <dailinfeng66@163.com>
2023-12-02 19:40:57 +0800

    update code

--- code/src/nohmm/model_params.py
+++ code/src/nohmm/model_params.py
@@ -7,7 +7,7 @@ class Config(object):
     # 学习率
     learning_rate = 2e-5
     # epoch数
-    num_train_epochs = 100
+    num_train_epochs = 50
     # 句子的最大补齐长度
     # sentence_max_len = 2048
     sentence_max_len = 256
@@ -15,9 +15,9 @@ class Config(object):
     predict_res_file = "/home/dlf/crf/code/res_files/short_data_res_{}.txt"
     # 词性的类别数量
     # jw
-    # class_nums = 18
+    class_nums = 18
     # ctb
-    class_nums = 42
+    # class_nums = 42
     # class_nums = 42
     # ud
     # class_nums = 15
@@ -47,11 +47,11 @@ class Config(object):
     few_shot = [5, 10, 15, 20, 25]
     # 测试集位置
     # jw
-    # train_data_path = "/home/dlf/crf/code/data/split_data/fold/{item}.data"
-    # test_data_path = "/home/dlf/crf/code/data/split_data/pos_seg_test.data"
+    train_data_path = "/home/dlf/crf/code/data/split_data/fold/{item}.data"
+    test_data_path = "/home/dlf/crf/code/data/split_data/pos_seg_test.data"
     # CTB
-    test_data_path = "/home/dlf/crf/code/data/ctb/split_data/few_shot/one_tentn_test_datas.data"
-    train_data_path = "/home/dlf/crf/code/data/ctb/split_data/few_shot/fold/{item}.data"
+    # test_data_path = "/home/dlf/crf/code/data/ctb/split_data/few_shot/one_tentn_test_datas.data"
+    # train_data_path = "/home/dlf/crf/code/data/ctb/split_data/few_shot/fold/{item}.data"
     # test_data_path = "/home/dlf/crf/code/data/ctb/split_data/few_shot/test_3000.data"
     # UD数据集
     # test_data_path = "/home/dlf/crf/code/data/ud/ud_en/test.data"
@@ -69,20 +69,20 @@ class Config(object):
     #                   "SP", "VV", "M", "PU", "CD", "BP", "JJ", "LC", "VC",
     #                   "VA", "VE"]
     # ctb数据集
-    special_labels = ["[PLB]", "NR", "NN", "AD", "PN", "OD", "CC", "DEG",
-                      "SP", "VV", "M", "PU", "CD", "BP", "JJ", "LC", "VC",
-                      "VA", "VE",
-                      "NT-SHORT", "AS-1", "PN", "MSP-2", "NR-SHORT", "DER",
-                      "URL", "DEC", "FW", "IJ", "NN-SHORT", "BA", "NT", "MSP", "LB",
-                      "P", "NOI", "VV-2", "ON", "SB", "CS", "ETC", "DT", "AS", "M", "X",
-                      "DEV"
-                      ]
+    # special_labels = ["[PLB]", "NR", "NN", "AD", "PN", "OD", "CC", "DEG",
+    #                   "SP", "VV", "M", "PU", "CD", "BP", "JJ", "LC", "VC",
+    #                   "VA", "VE",
+    #                   "NT-SHORT", "AS-1", "PN", "MSP-2", "NR-SHORT", "DER",
+    #                   "URL", "DEC", "FW", "IJ", "NN-SHORT", "BA", "NT", "MSP", "LB",
+    #                   "P", "NOI", "VV-2", "ON", "SB", "CS", "ETC", "DT", "AS", "M", "X",
+    #                   "DEV"
+    #                   ]
     # UD 数据集
     # special_labels = ["[PLB]","PROPN", "SYM", "X", "PRON", "ADJ", "NOUN", "PART", "DET", "CCONJ", "ADP", "VERB", "NUM", "PUNCT", "AUX", "ADV"]
     # 检查点的保存位置
     checkpoint_file = "/home/dlf/crf/code/src/crf/pths/ud-ch_{filename}.pth"
     pretrain_models = [
-        "/home/dlf/crf/code/model/bert_large_chinese",
-        #"/home/dlf/crf/code/model/medbert",
-        # "/home/dlf/crf/code/model/bart-large"
+        "/home/dlf/prompt/code/model/bert_large_chinese",
+        "/home/dlf/prompt/code/model/medbert",
+        "/home/dlf/prompt/code/model/bart-large"
     ]
