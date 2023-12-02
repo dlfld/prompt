fa28d37089b32650cd603356659a196d5733be42
戴林峰 <dailinfeng66@163.com>
2023-12-02 17:20:30 +0800

    update code

--- code/src/data_process/data_processing.py
+++ code/src/data_process/data_processing.py
@@ -111,7 +111,7 @@ def generate_prompt(sentence: str, word: str, pre_part_of_speech: str, pre_word:
     :param pre_word: 前一个词语
     :param part_of_speech: 当前词语的词性
     """
-    template = "在句子“{sentence}”中，词语“{word}”的前文如果是由“{pre_part_of_speech}”词性的词语“{pre_word}”来修饰，那么词语“{word}”的词性是“[MASK]”，从词性标签集中选择，词性标签集为：[{labels}]→ {part_of_speech}"
+    template = "在句子“{sentence}”中，词语“{word}”的前文如果是由“{pre_part_of_speech}”词性的词语“{pre_word}”来修饰，那么词语“{word}”的词性是“[MASK]”，从结果集中选择[{labels}]，→ {part_of_speech}"
     template2 =  "句子“{sentence}”中，“{word}”是由“{pre_part_of_speech}”词性的词语“{pre_word}”来修饰，“{word}”的词性是“[MASK]”→ {part_of_speech}"
     template3 =  "句子“{sentence}”中，“{word}”的词性是“[MASK]”→ {part_of_speech}"
     labels = ",".join(config.special_labels[1:])
