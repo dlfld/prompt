6f68de65e08232f0faab815510ff3be934ee0d57
titan <dailinfeng66@163.com>
2024-01-08 11:35:09 +0800

    update code

--- code/src/prompt/model_params.py
+++ code/src/prompt/model_params.py
@@ -17,9 +17,9 @@ class Config(object):
     predict_res_file = "/home/dlf/prompt/code/res_files/short_data_res_{}.txt"
     # 词性的类别数量
     # class_nums = 18
-    class_nums = 42
+    #class_nums = 42
     # ud
-    # class_nums = 15
+    class_nums = 15
     # 计算使用的device
     # device = "cpu"
     device = "cuda:0"
@@ -44,12 +44,12 @@ class Config(object):
     # train_data_path = "/home/dlf/prompt/code/data/split_data/fold/{item}.data"
     # test_data_path = "/home/dlf/prompt/code/data/split_data/pos_seg_test.data"
     # CTB
-    test_data_path = "/home/dlf/prompt/code/data/ctb/split_data/few_shot/one_tentn_test_datas.data"
-    train_data_path = "/home/dlf/prompt/code/data/ctb/split_data/few_shot/fold/{item}.data"
+    #test_data_path = "/home/dlf/prompt/code/data/ctb/split_data/few_shot/one_tentn_test_datas.data"
+    #train_data_path = "/home/dlf/prompt/code/data/ctb/split_data/few_shot/fold/{item}.data"
     # test_data_path = "/home/dlf/prompt/code/data/ctb/split_data/few_shot/test_3000.data"
     # UD
-    # test_data_path = "/home/dlf/prompt/code/data/ud/ud_en/test.data"
-    # train_data_path = "/home/dlf/prompt/code/data/ud/ud_en/fold/{item}.data"
+    test_data_path = "/home/dlf/prompt/code/data/ud/ud_en/test.data"
+    train_data_path = "/home/dlf/prompt/code/data/ud/ud_en/fold/{item}.data"
     # log dir
     log_dir = "ud_bert_medbert_bert/"
     # train dataset template
@@ -63,17 +63,17 @@ class Config(object):
     #                   "SP", "VV", "M", "PU", "CD", "BP", "JJ", "LC", "VC",
     #                   "VA", "VE"]
     # ctb数据集
-    special_labels = ["[PLB]", "NR", "NN", "AD", "PN", "OD", "CC", "DEG",
-                    "SP", "VV", "M", "PU", "CD", "BP", "JJ", "LC", "VC",
-                   "VA", "VE",
-                  "NT-SHORT", "AS-1", "PN", "MSP-2", "NR-SHORT", "DER",
-                 "URL", "DEC", "FW", "IJ", "NN-SHORT", "BA", "NT", "MSP", "LB",
-                "P", "NOI", "VV-2", "ON", "SB", "CS", "ETC", "DT", "AS", "M", "X",
-               "DEV"
-              ]
+    #special_labels = ["[PLB]", "NR", "NN", "AD", "PN", "OD", "CC", "DEG",
+     #               "SP", "VV", "M", "PU", "CD", "BP", "JJ", "LC", "VC",
+     #              "VA", "VE",
+     #             "NT-SHORT", "AS-1", "PN", "MSP-2", "NR-SHORT", "DER",
+     #            "URL", "DEC", "FW", "IJ", "NN-SHORT", "BA", "NT", "MSP", "LB",
+     #           "P", "NOI", "VV-2", "ON", "SB", "CS", "ETC", "DT", "AS", "M", "X",
+     #          "DEV"
+    #          ]
     # UD 数据集
-    # special_labels = ["[PLB]", "PROPN", "SYM", "X", "PRON", "ADJ", "NOUN", "PART", "DET", "CCONJ", "ADP", "VERB", "NUM",
-    #                   "PUNCT", "AUX", "ADV"]
+    special_labels = ["[PLB]", "PROPN", "SYM", "X", "PRON", "ADJ", "NOUN", "PART", "DET", "CCONJ", "ADP", "VERB", "NUM",
+                       "PUNCT", "AUX", "ADV"]
     # 检查点的保存位置
     checkpoint_file = "/home/dlf/prompt/code/src/prompt/pths/ud-ch_{filename}.pth"
     pretrain_models = [
