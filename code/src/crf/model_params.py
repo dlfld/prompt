28ec8a16c47f9f7bd2ca3ae117a3b2736171968e
戴林峰 <dailinfeng66@163.com>
2024-02-22 12:30:28 +0800

    update code

--- code/src/crf/model_params.py
+++ code/src/crf/model_params.py
@@ -2,66 +2,85 @@ class Config(object):
     """
         配置类，保存配置文件
     """
-    dataset = "ud"
-
     # batch_size
-    batch_size = 8
+    batch_size = 16
+    # 连续提示块的数量
+
     # 学习率
+    head_lr = 2e-5
     learning_rate = 2e-5
+    hmm_lr = 0.01
     # epoch数
-    num_train_epochs = 100
+    num_train_epochs = 150
+    embed_size = 1024
     # 句子的最大补齐长度
     # sentence_max_len = 2048
     sentence_max_len = 128
     # 结果文件存储位置
-    predict_res_file = "/home/dlf/p-tuning/code/res_files/short_data_res_{}.txt"
+    predict_res_file = "/home/dlf/prompt/code/res_files/short_data_res_{}.txt"
     # 词性的类别数量
-    # jw
-    # class_nums = 18
-    # ctb
+    class_nums = 18
     # class_nums = 42
     # ud
-    class_nums = 15
+    # class_nums = 15
     # 计算使用的device
     # device = "cpu"
-    device = "cuda:0"
+
     # k折交叉验证
-    kfold = 1
+    kfold = 5
+    template1 = "在句子“{sentence}”中，词语“{word}”的前文如果是由“{pre_part_of_speech}”词性的词语“{pre_word}”来修饰，那么词语“{word}”的词性是“[MASK]”→ {part_of_speech}"
+    template7 = "词语“{word}”的前文如果是由“{pre_part_of_speech}”词性的词语“{pre_word}”来修饰，在句子“{sentence}”中，那么词语“{word}”的词性是“[MASK]”→ {part_of_speech}"
+    template8 = "在句子“{sentence}”中，词语“{word}”的词性是“[MASK]”,如果词语“{word}”的前文是由“{pre_part_of_speech}”词性的词语“{pre_word}”来修饰。→ {part_of_speech}"
+    template9 = "在句子“{sentence}”中，词语“{pre_word}的词性是{pre_part_of_speech}”，那么词语“{word}”的词性是“[MASK]”→ {part_of_speech}"
+    template_pt = "[T]{sentence}[T]{word}[T]{pre_part_of_speech}[T]{pre_word}[T]{word}[T][MASK]→ {part_of_speech}"
 
-
-    # ctb dataset
-    # train_1_9_path = "/home/dlf/p-tuning/code/data/ctb/split_data/1_9_split/train_{idx}.data"
-    # test_1_9_path = "/home/dlf/p-tuning/code/data/ctb/split_data/1_9_split/test_{idx}.data"
+    template = template1
 
     # 是否断点续训
     resume = False
     # few-shot 划分的数量
-    few_shot = [5, 10, 15, 20, 25, 50, 75, 100, 200, 500]
-    # few_shot = [20, 70]
-
+    few_shot = [5, 10, 15, 20, 25]
     # 测试集位置
-    # test_data_path = "/home/dlf/p-tuning/code/data/ctb/split_data/few_shot/ctb_test.data"
-    # test_data_path = "/home/dlf/p-tuning/code/data/ctb/split_data/few_shot/one_tentn_test_datas.data"
-    # train dataset template
-    # train_data_path = "/home/dlf/p-tuning/code/data/ctb/split_data/few_shot/fold/{item}.data"
-    
-    test_data_path = "/home/dlf/p-tuning/code/data/ud/ud_en/test.data"
-    train_data_path = "/home/dlf/p-tuning/code/data/ud/ud_en/fold/{item}.data"
+    # jw
+    train_data_path = "/home/dlf/prompt/code/data/split_data/fold/{item}.data"
+    test_data_path = "/home/dlf/prompt/code/data/split_data/pos_seg_test.data"
+    # CTB
+    # test_data_path = "/home/kdwang/dlf/prompt/code/data/ctb/split_data/few_shot/one_tentn_test_datas.data"
+    # train_data_path = "/home/kdwang/dlf/prompt/code/data/ctb/split_data/few_shot/fold/{item}.data"
+    # test_data_path = "/home/dlf/prompt/code/data/ctb/split_data/few_shot/test_3000.data"
+    # UD
+    # test_data_path = "/home/dlf/prompt/code/data/ud/ud_en/test.data"
+    # train_data_path = "/home/dlf/prompt/code/data/ud/ud_en/fold/{item}.data"
     # log dir
-    log_dir = "ud_bert_medbert_bart/"
+    log_dir = "ud_bert_medbert_bert/"
+    # train dataset template
+
+    # train_data_path = "/home/dlf/prompt/code/data/split_data/{item}.data"
+    # 截取句子的前n个词组成prompt,超过8个要oom
+    # pre_n = 8
     # label
-    # ctb
+    # jw 数据集
+    special_labels = ["[PLB]", "NR", "NN", "AD", "PN", "OD", "CC", "DEG",
+                      "SP", "VV", "M", "PU", "CD", "BP", "JJ", "LC", "VC",
+                      "VA", "VE", "[T]"]
+    # ctb数据集
     # special_labels = ["[PLB]", "NR", "NN", "AD", "PN", "OD", "CC", "DEG",
-    #                   "SP", "VV", "M", "PU", "CD", "BP", "JJ", "LC", "VC",
-    #                   "VA", "VE",
-    #                   "NT-SHORT", "AS-1", "PN", "MSP-2", "NR-SHORT", "DER",
-    #                   "URL", "DEC", "FW", "IJ", "NN-SHORT", "BA", "NT", "MSP", "LB",
-    #                   "P", "NOI", "VV-2", "ON", "SB", "CS", "ETC", "DT", "AS", "M", "X",
-    #                   "DEV"
-    #                   ]
-    special_labels = ["[PLB]","PROPN", "SYM", "X", "PRON", "ADJ", "NOUN", "PART", "DET", "CCONJ", "ADP", "VERB", "NUM", "PUNCT", "AUX", "ADV"]
+    #              "SP", "VV", "M", "PU", "CD", "BP", "JJ", "LC", "VC",
+    #                 "VA", "VE",
+    #                "NT-SHORT", "AS-1", "PN", "MSP-2", "NR-SHORT", "DER",
+    #               "URL", "DEC", "FW", "IJ", "NN-SHORT", "BA", "NT", "MSP", "LB",
+    #              "P", "NOI", "VV-2", "ON", "SB", "CS", "ETC", "DT", "AS", "M", "X",
+    #             "DEV"
+    #       ]
+    # UD 数据集
+    # special_labels = ["[PLB]", "PROPN", "SYM", "X", "PRON", "ADJ", "NOUN", "PART", "DET", "CCONJ", "ADP", "VERB", "NUM",
+    #                  "PUNCT", "AUX", "ADV"]
+    # 检查点的保存位置
+    checkpoint_file = "/home/dlf/prompt/code/src/prompt/pths/ud-ch_{filename}.pth"
+    device = "cuda:0"
+    # device = "cpu"
     pretrain_models = [
-        "/home/dlf/p-tuning/code/model/bert_large_chinese",
-        "/home/dlf/p-tuning/code/model/medbert",
-        "/home/dlf/p-tuning/code/model/bart-large"
+        # "/home/dlf/prompt/code/model/bert_large_chinese",
+        "/home/dlf/prompt/code/model/medbert",
+        # "/home/dlf/prompt/code/model/bart-large"
     ]
--- code/src/crf/models.py
+++ code/src/crf/models.py
@@ -1,16 +1,17 @@
-import logddd
+import sys
+
 import torch
+import torch.nn.functional as F
 from torch import nn
 from torchcrf import CRF
-import sys
-import torch.nn.functional as F
+import logddd
 
 sys.path.append("..")
 from model_params import Config
 
 
 class CRFModel(nn.Module):
-    def __init__(self, bert_model, class_nums, tokenizer):
+    def __init__(self, bert_model, class_nums, tokenizer, bert_config, model_checkpoint):
         """
             @param bert_model: 预训练模型
             @param hidden_size: 隐藏层大小
@@ -22,44 +23,74 @@ class CRFModel(nn.Module):
         self.bert = bert_model
         # 标签的类别数量
         self.class_nums = class_nums
-        self.crf = CRF(num_tags=Config.class_nums, batch_first=True)
-        # tokenizer
+
+        self.embed_size = 0
+        if "bert_" in model_checkpoint or "bart" in model_checkpoint:
+            self.embed_size = 1024
+        else:
+            self.embed_size = 768
+
+        self.model_checkpoint = model_checkpoint
+
+        self.fc = nn.Linear(self.embed_size, Config.class_nums)
+
         self.tokenizer = tokenizer
+
+        self.loss_fct = torch.nn.CrossEntropyLoss()
+        self.crf = CRF(self.class_nums, batch_first=True)
 
     def forward(self, datas):
         # logddd.log(datas)
-        output = self.bert(**datas)
-        loss = output.loss
-        # batch 128 21128
-        logits = output.logits
-        # 记录那些位置是填充进取的
-        masks = []
+        inputs = {
+            "input_ids": datas["input_ids"],
+            "attention_mask": datas["attention_mask"],
+        }
+        if "bart" in self.model_checkpoint:
+            output = self.bert(**inputs).last_hidden_state
+        else:
+            output = self.bert(**inputs)[0]
 
-        for mask in datas["attention_mask"]:
-            masks.append(mask.tolist())
-        # 转换为tensor
-        masks_crf = torch.tensor(masks, dtype=torch.bool).to(Config.device)
+        # 5 128 18
+        seq_out = self.fc(output)
 
-        # bert的输出是21128维的，截取词性标签在词表中index的那一段，拿出来用
-        res_logits = logits[:, :, 1:1 + Config.class_nums]
-        # logddd.log(res_logits.shape)
-        # exit(0)
-        # 将label中填充的-100转换成0，因为crf中只有设置的label数量，放-100进取会报错
-        labels = []
-        for sentence in datas["labels"]:
-            item = [x - 1 if x != -100 else 0 for x in sentence.tolist()]
-            labels.append(item)
-        # 写回label
-        datas["labels"] = torch.tensor(labels).to(Config.device)
+        labels = datas["labels"] if "labels" in datas.keys() else None
 
-        crf_loss = self.crf(res_logits, datas["labels"], mask=masks_crf, reduction="mean")
-        # 将bert的loss和crf的loss加起来，因为crfloss是负对数似然函数，因此在这个地方取负
-        total_loss = loss - crf_loss
-        # 获取crf计算出来的最优路径
-        decode = self.crf.decode(res_logits, mask=masks_crf)
-        # 预测出来的label,和真实label之间相差1，因为在词表当中，真实label的id是从1开始，因此需要加1
-        predict_labels = []
-        for sequence in decode:
-            predict_labels.append([x + 1 for x in sequence])
+        path = self.crf.decode(seq_out, mask=datas["attention_mask"].bool())
+        res_paths = [x + 1 for row in path for x in row]
+        loss = 0
+        if labels is not None:
+            label = []
+            for sentence in datas["labels"]:
+                # item = [x - 1 if x != -100 else 0 for x in sentence.tolist()]
+                # 模型加载的label是比原始的大1
+                item = [x - 1 if x != -100 else 0 for x in sentence.tolist()]
+                label.append(item)
 
-        return total_loss/len(datas), predict_labels
+            loss = -self.crf(seq_out, torch.tensor(label).to(Config.device), mask=datas["attention_mask"].byte(),
+                             reduction='mean')
+        return 0, loss, res_paths
+
+    def get_loss(self, logits, labels):
+        outputs = logits.view(-1, self.class_nums)
+        label = labels.view(-1)
+        for i in range(len(label)):
+            if label[i] != -100:
+                label[i] = label[i] - 1
+        masked_lm_loss = self.loss_fct(outputs, labels.view(-1))
+        return masked_lm_loss
+
+    def get_paths(self, logits, labels):
+        # logddd.log(logits.shape)
+        total_labels = []
+        for s_idx, sentence in enumerate(labels):
+            for w_idx, item in enumerate(sentence):
+                if item != -100:
+                    total_labels.append(logits[s_idx][w_idx].tolist())
+
+        probabilities = F.softmax(torch.tensor(total_labels).to(Config.device))
+
+        predictions = torch.argmax(probabilities, dim=1)
+        predictions = predictions.tolist()
+        predictions = [x + 1 for x in predictions]
+        return predictions
+
